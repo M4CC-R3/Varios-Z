7d15f115c6f22746a8c3b9b08d4aefb418682a2e
M4 <mcubillascr@uniminuto.co>
2019-01-08 19:04:32 -0500

    Varios métodos en tuplas,list and dict

--- WxGraphics.py
+++ WxGraphics.py
@@ -1,4 +1,14 @@
+import nmap
+import hashlib
+
+
+print(hashlib.algorithms_available)
+
+print(hash("mighty"))
+
 val = input("Digite el número o palabra  a convertir a binario: ")
+
+
 
 confirmador = str.isdecimal(val)
 
@@ -6,17 +16,34 @@ if (confirmador == True):
 
 	print((bin(int(val))).replace('b',''))
 
-if (confirmador == False):
+else:
 
 	for l in val:
 		print((bin(ord(l)).replace('b','')), end='')
-
-import nmap
+"""
 nmScan = nmap.PortScanner()
 nmScan.scan('216.58.222.196', '21-443')
 
 
 nmScan.command_line()
-for x in range(21,444):
-	print("Host google con puerto: ",x,": ",nmScan['216.58.222.196'].has_tcp(x))
+
+lista = (54,2,41,5,"D","DS")
+
+#print(list(enumerate(lista)))
+"""
+#print(hex(10))
+#print(str(41) in str(41))
+print("\n ",str.isalnum("jhn0"))
+print("\n {0}".format(str.isdigit("541.2")))
+#print("HOLA")
+
+
+Hi = frozenset([54,51,54,51,"dsd","dsd"])
+
+Hi2 = {"SD",510,54}
+print("\n Theren't elements common: {0}".format(Hi2.isdisjoint(Hi)))
+
+print("Every the element is on the set: ",Hi2.issubset(Hi))
+
+print(Hi.difference(Hi2))
 
