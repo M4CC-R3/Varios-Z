ee63ca17e9adcfedb25763615e30d49a57cb0412
M4 <mcubillascr@uniminuto.co>
2019-01-14 15:32:18 -0500

    Calcular edad de forma exacta

--- newM4.py
+++ newM4.py
@@ -3,8 +3,13 @@ from datetime import date
 from datetime import timedelta
 
 hoy = datetime.now()
+try:
+    fechaNacimiento = input("Digite su fecha de nacimiento (YY-MM-DD): ")
 
-fechaNacimiento = input("Digite su fecha de nacimiento (YY-MM-DD): ")
+except ValueError:
+    print("Valores erroneos, verifique que halla digita"
+          " el formato correcto (AÑO:MES:DIA)")
+    exit()
 
 fechaNacimiento = fechaNacimiento.split("-")
 
@@ -12,12 +17,20 @@ fechaNacimiento = date(int(fechaNacimiento[0]),int(fechaNacimiento[1]),int(fecha
 fechaActual = date.today()
 
 restoDelta = timedelta((fechaActual-fechaNacimiento).days-396)
-final = (str(restoDelta+date(1,1,1))).split("-")
+try:
+    final = (str(restoDelta+date(1,1,1))).split("-")
+except OverflowError:
+    print("Error de fechas")
+    exit()
 
 año = int(final[0])
 mes = int(final[1])
 dia = int(final[2])
 
-print("Su edad es de: {0} Años, {0} Meses y {0} Dias".format(año,mes,dia))
+if mes == 12:
+    año += 1
+    mes = 0
+
+print("Su edad es de: {0} Años, {1} Meses y {2} Dias".format(año,mes,dia))
 
 print(datetime.now()-hoy)
