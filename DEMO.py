b9d51308204b352c24d13cd0acb4ddaef193ebe2
M4 <M4F>
2019-01-26 18:42:53 -0500

    n

--- DEMO.py
+++ DEMO.py
@@ -7,18 +7,10 @@ lista = [2,5,4,7,9,9,12,2,3,5,1,0,231,5,7,9,5,8,4,0,3,21]
 
 lista.sort()
 
-lista.append(80)
-lista.sort()
-lista.append(60)
-lista.sort()
-lista.append(10)
-lista.sort()
-lista.append(20)
-lista.sort()
-lista.append(30)
-lista.sort()
-lista.append(5)
-lista.sort()
+bisect.insort(lista,100)
+bisect.insort(lista,101)
+bisect.insort(lista,81)
+
 
 print(lista)
 
