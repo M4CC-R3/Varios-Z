921dcd602e27a5d0e7f994c1eb758597e34d8664
M4 <M4F>
2019-01-30 14:15:50 -0500

    Comparación entre listas con difflib.unified

--- all-2.py
+++ all-2.py
@@ -1,28 +1,11 @@
 import difflib
 import sys
+l1 = ["HOLA","Mundo","mango","Fruta"]
+l2 = ["HOLA","MUNDO","mango","carnes"]
 
-texto1 = """Antigua Roma:
-Antigua Roma (en latín, Antiqua Rōma) designa la
-entidad política unitaria surgida de la expansión de la ciudad
-de Roma, que en su época de apogeo, llegó a abarcar desde Gran
-Bretaña al desierto del Sahara y desde la península ibérica al
-Éufrates. En un principio, tras su fundación (según la
-tradición en 753 aC), Roma fue una monarquía etrusca. Más
-tarde (509 aC) fue una república latina, y en 27 aC
-se convirtió en un imperio."""
+l1f = list(map((lambda x : x+"\n"),l1))
+l2f = list(map((lambda x : x+"\n"),l2))
 
-texto2 = """Antigua Roma (en latín, Antiqua Rōma) designa la
-entidad política unitaria surgida de la expansión de la ciudad
-de Roma, que en su época de apogeo, llegó a abarcar desde Gran
-Bretaña al desierto del Sahara y desde la península ibérica al
-Éufrates. En un principio, tras su creación (según la
-tradición en 753 a. C.), Roma fue una monarquía etrusca. Más
-tarde (509 a. C.) fue una república latina, y en 27 a. C.
-se convirtió en un imperio"""
+comparation = difflib.unified_diff(l1f,l2f,fromfile="a.py",tofiledate="b.py")
 
-text1c = texto1.splitlines()
-text2c = texto2.splitlines()
-
-comparador = difflib.Differ().compare(text1c,text2c)
-
-sys.stdout.writelines("\n".join(comparador))
+sys.stdout.writelines(comparation)
